44dd0c0f94fdc6ed2c4db7855b9d643900622b68
EC2 Default User <ec2-user@ip-172-31-9-231.ec2.internal>
2017-12-02 05:39:09 +0000

    got the call to get the lead working all the way from close to the API

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-exports.handler = function (event, context) {
-    console.log('Received event:', event)
-    context.done(null, {status: 200, body: "bacon"})
+'use strict';
+
+console.log('Loading function');
+
+const https = require('https');
+
+
+var apiKey = process.env.API_KEY;
+var leadId = null;
+
+exports.handler = (event, context, callback) => {
+    leadId = event.leadId;
+    console.log("leadId: " + leadId);
+    getLead((result) => callback(null, result));
+};
+
+function constructOptions() {
+    return {
+        host: "app.close.io",
+        path: "/api/v1/lead/" + leadId + "/",
+        auth: apiKey + ":"
+    };
+}
+
+function getLead(callback) {
+    var options = constructOptions();
+
+    return https.get(options, function(response) {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            console.log("status: " + response.statusCode);
+            console.log("body: \n" + body);
+            console.log("\n\n\n");
+            callback({
+                statusCode: response.statusCode,
+                body: JSON.parse(body),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        });
+    });
 }
